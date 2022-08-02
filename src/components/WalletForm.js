1ec35fdde77bae0f1af4e08a2be5653c1fd093c7
italo21211 <italoarauju2019@gmail.com>
2022-08-02 18:22:36 -0300

    Desafio 6 completo

--- src/components/WalletForm.js
+++ src/components/WalletForm.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import Table from './Table';
 import '../pages/formWalet.css';
 import { getApi as getApiFetch,
   getExpensesFetch as getExpensesAction } from '../redux/actions';
@@ -41,76 +42,82 @@ class WalletForm extends Component {
     const { currencies } = this.props;
     const { value, description, currency } = this.state;
     return (
-      <div className="container-form">
-        <label htmlFor="valor-input">
-          <span>Valor:</span>
-          <input
+      <div>
+        <div className="container-form">
+          <label htmlFor="valor-input">
+            <span>Valor:</span>
+            <input
+              className="input-valor "
+              id="valor-input"
+              data-testid="value-input"
+              type="number"
+              name="value"
+              value={ value }
+              onChange={ this.handChange }
+            />
+          </label>
+
+          <span>Moeda:</span>
+          <select
             className="input-valor "
-            id="valor-input"
-            data-testid="value-input"
-            type="number"
-            name="value"
-            value={ value }
+            data-testid="currency-input"
             onChange={ this.handChange }
-          />
-        </label>
+            name="currency"
+            value={ currency }
+          >
+            {currencies
+              .map((moedas) => <option value={ moedas } key={ moedas }>{moedas}</option>)}
+          </select>
 
-        <span>Moeda:</span>
-        <select
-          className="input-valor "
-          data-testid="currency-input"
-          onChange={ this.handChange }
-          name="currency"
-          value={ currency }
-        >
-          {currencies
-            .map((moedas) => <option value={ moedas } key={ moedas }>{moedas}</option>)}
-        </select>
-
-        <label htmlFor="valor-input">
-          <span>Descrição:</span>
-          <input
+          <label htmlFor="valor-input">
+            <span>Descrição:</span>
+            <input
+              className="inputs"
+              id="valor-input"
+              type="text"
+              data-testid="description-input"
+              name="description"
+              value={ description }
+              onChange={ this.handChange }
+            />
+          </label>
+          <span>Metodo de pagamento:</span>
+          <select
             className="inputs"
-            id="valor-input"
-            type="text"
-            data-testid="description-input"
-            name="description"
-            value={ description }
+            data-testid="method-input"
             onChange={ this.handChange }
-          />
-        </label>
-        <span>Metodo de pagamento:</span>
-        <select
-          className="inputs"
-          data-testid="method-input"
-          onChange={ this.handChange }
-          name="method"
-        >
-          <option>Dinheiro</option>
-          <option>Cartão de crédito</option>
-          <option>Cartão de débito</option>
-        </select>
-        <span>Tag:</span>
-        <select
-          className="inputs"
-          data-testid="tag-input"
-          onChange={ this.handChange }
-          name="tag"
-        >
-          <option>Alimentação</option>
-          <option>Lazer</option>
-          <option>Trabalho</option>
-          <option>Transporte</option>
-          <option>Saúde</option>
-        </select>
+            name="method"
+          >
+            <option>Dinheiro</option>
+            <option>Cartão de crédito</option>
+            <option>Cartão de débito</option>
+          </select>
+          <span>Tag:</span>
+          <select
+            className="inputs"
+            data-testid="tag-input"
+            onChange={ this.handChange }
+            name="tag"
+          >
+            <option>Alimentação</option>
+            <option>Lazer</option>
+            <option>Trabalho</option>
+            <option>Transporte</option>
+            <option>Saúde</option>
+          </select>
 
-        <button
-          className="inputSub"
-          type="button"
-          onClick={ this.addDespesas }
-        >
-          Adicionar despesa
-        </button>
+          <button
+            className="inputSub"
+            type="button"
+            onClick={ this.addDespesas }
+          >
+            Adicionar despesa
+          </button>
+        </div>
+        <div>
+          <Table />
+        </div>
+
       </div>
     );
   }
